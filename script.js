80fbf03f4169cc87ebc590ced08cdff225024eef
Filip Gabrys <el.filberto@gmail.com>
2022-03-13 12:43:55 +0100

    fix scipt.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'https://kodilla-tasks-fg.herokuapp.com/v1/tasks;
+  var apiRoot = 'https://kodilla-tasks-fg.herokuapp.com/v1/tasks';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
